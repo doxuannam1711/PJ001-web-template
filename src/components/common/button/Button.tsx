980f765cb75d69729735e67ad962b4388e1e27f5
SANG\phamq <phamquangsang661@gmail.com>
2023-04-20 01:10:15 +0700

    fix: param prop for button

--- src/components/common/button/Button.tsx
+++ src/components/common/button/Button.tsx
@@ -6,9 +6,9 @@ export interface Button {
     children: ReactNode;
     props: React.DetailedHTMLProps<React.ButtonHTMLAttributes<HTMLButtonElement>, HTMLButtonElement>
 }
-export function Button({ className = "", textClassName = "", children, ...props }: Button) {
+export function Button({ className = "", textClassName = "", children, props }: Button) {
 
-    return <button  {...props.props}
+    return <button  {...props}
         className={`bg-primary-main hover:bg-primary-dark active:bg-primary-light rounded-[24px] ${className}`}>
         <p className={`text-primary-contrast text-[17px] leading-[21px] font-medium text-center ${textClassName}`}>{children}</p>
     </button>
--- src/components/common/button/ButtonOutline.tsx
+++ src/components/common/button/ButtonOutline.tsx
@@ -6,8 +6,8 @@ export interface ButtonOutline {
     children: ReactNode;
     props: React.DetailedHTMLProps<React.ButtonHTMLAttributes<HTMLButtonElement>, HTMLButtonElement>
 }
-export function ButtonOutline({ className = "", textClassName = "", children, ...props }: ButtonOutline) {
-    return <button  {...props.props}
+export function ButtonOutline({ className = "", textClassName = "", children, props }: ButtonOutline) {
+    return <button  {...props}
         className={`border-[2px] hover:border-[0px] border-primary-light hover:bg-primary-light text-primary-default hover:text-primary-contrast rounded-[24px] ${className}`}>
         <p className={` text-[17px] leading-[21px] font-bold text-center ${textClassName}`}>{children}</p>
     </button>
