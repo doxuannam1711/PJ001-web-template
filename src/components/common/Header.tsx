ad89e07d7237779f027d322be1ae113630aec5f7
DevCuongBui <cuong82434@st.vimaru.edu.vn>
2023-04-20 16:16:27 +0700

    update: footer header logo2

--- src/components/common/Header.tsx
+++ src/components/common/Header.tsx
@@ -1,4 +1,4 @@
-import { Cursor, Logo2 } from "@components/vector"
+import { Logo2 } from "@components/vector"
 import { useState } from "react"
 
 export interface Header {
@@ -35,13 +35,7 @@ export function Header({ }: Header) {
 
             <div className="hidden max-w-[1440px] h-[69px] px-180 py-13 sm:gap-[200px] lg:gap-[490px] justify-between flex-row items-center md:flex">
                 <a className="flex" href="/" title="logo">
-                    {/* <Cursor size={43} className=""></Cursor> */}
-                    <Logo2 size={43} className=""></Logo2>
-                    <div className="ml-[6.8px] flex flex-col w-[42.26px] h-[43px] text-[8px] justify-center font-semibold">
-                        <p>INFORMATION</p>
-                        <p>CONSULTING</p>
-                        <p>GROUP</p>
-                    </div>
+                    <Logo2 size={130} textFillColor="black" className=""></Logo2>
                 </a>
                 <div className="">
                     <ul className="flex items-center gap-x-[24px]">
